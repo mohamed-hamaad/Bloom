f30df532754eacbf44cca1e8aa1905de56643e2c
Mohamed Hamaad <mohamednaderhamaad@gmail.com>
2026-06-22 14:39:01 +0300

    Add LOGIN_URL setting to Django settings
    Added LOGIN_URL setting for user authentication.

--- Bloom/settings.py
+++ Bloom/settings.py
@@ -26,9 +26,12 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-glm0fdhc0ecv#w
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1', '.vercel.app'
+    'localhost', '127.0.0.1','.railway.app''
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.railway.app',
+]
 
 # Application definition
 
